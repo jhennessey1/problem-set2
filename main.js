fe5518977bb20c50ee09b7e712cc6e5c9e0f9c02
Student <student@RU-005.local>
2016-01-26 14:19:40 -0700

    all functional

--- main.js
+++ main.js
@@ -49,38 +49,80 @@ function swapCase(string) {
 // }
 
 
-function letterCount(string) {
-	stringArray = string.split(" ")
-	var count = 0
-	var mostRepeats = ""
-	for(var i = 0; i < stringArray.length; i++) {
-		var lettersArray = stringArray[i].split("")
-		for(var n = 0; n < lettersArray.length; n++) {
-			var innerCount = 0
-			var repeatedLetters = []
-			for(var x = (n + 1); x < lettersArray.length; x++) {
-				repeatedLetters.push(lettersArray[n])
-				if(lettersArray[n] === lettersArray[x]) {
-					repeatedLetters.push(lettersArray[x])
-				}
-			}
-			if(repeatedLetters.length > innerCount) {
-				innerCount = repeatedLetters.length
-				repeatedLetters = []
+// function letterCount(string) {
+// 	stringArray = string.split(" ")
+// 	var count = 0
+// 	var mostRepeats = ""
+// 	for(var i = 0; i < stringArray.length; i++) {
+// 		var lettersArray = stringArray[i].split("")
+// 		for(var n = 0; n < lettersArray.length; n++) {
+// 			var innerCount = 0
+// 			var repeatedLetters = []
+// 			for(var x = (n + 1); x < lettersArray.length; x++) {
+// 				repeatedLetters.push(lettersArray[n])
+// 				if(lettersArray[n] === lettersArray[x]) {
+// 					repeatedLetters.push(lettersArray[x])
+// 				}
+// 			}
+// 			if(repeatedLetters.length > innerCount) {
+// 				innerCount = repeatedLetters.length
+// 				repeatedLetters = []
+// 			}
+// 		}
+// 		if(innerCount > count) {
+// 			count = innerCount
+// 			innerCount = 0
+// 			mostRepeats = stringArray[n]
+// 		}
+// 	}
+// 	return mostRepeats
+// }
+
+var repeatLettersCount = function(word) {
+	count = -1
+	for(i = 0; i < word.length; i++) {
+		innerCount = -1
+		for(n = 0; n < word.length; n++) {
+			if(word[n] === word[i]) {
+				innerCount++
 			}
 		}
 		if(innerCount > count) {
 			count = innerCount
-			innerCount = 0
-			mostRepeats = stringArray[n]
+			innerCount = -1
+		} 
+		else {
+			innerCount = -1
 		}
 	}
-	return mostRepeats
+	return count
 }
 
 
 
 
+var letterCount = function(string) {
+	var stringArray = string.split(" ")
+	var mostRepeats = ""
+	var count = 0
+	for(var i = 0; i < stringArray.length; i++) {
+		if(repeatLettersCount(stringArray[i]) > count) {
+			count = repeatLettersCount(stringArray[i])
+			mostRepeats = stringArray[i]
+		}
+	}
+	if(mostRepeats.length > 0) {
+		return mostRepeats
+	}
+	else {
+		return -1
+	}
+}
+
+
+var test = letterCount("aa")
+
+console.log(test)
 
 
 
