cc753c99517d5c9ddf64c3fe52d744277742a106
Student <student@RU-005.local>
2016-01-26 09:01:32 -0700

    bonus problem changes

--- main.js
+++ main.js
@@ -24,28 +24,68 @@ function swapCase(string) {
 	return newString.join("")
 }
 
+// function letterCount(string) {
+// 	var count = 0
+// 	var mostRepeats = ""
+// 	var stringArray = string.split(" ")
+// 	for(i = 0; i < stringArray.length; i++) {
+// 		var innerCount = 0
+// 		var letters = stringArray[i].split("")
+// 		for(n = 0; n < letters.length; n++) {
+// 			for(x = (n + 1); x < letters.length; x++) {
+// 				if(letters[n] === letters[x]) {
+// 					innerCount += 1
+// 				}
+				
+// 			}
+// 			if(innerCount > count) {
+// 			count = innerCount
+// 			innerCount = 0
+// 			mostRepeats = stringArray[i]
+// 		}	
+// 	}
+// 	return mostRepeats
+// 	}
+// }
+
+
 function letterCount(string) {
+	stringArray = string.split(" ")
 	var count = 0
 	var mostRepeats = ""
-	var stringArray = string.split(" ")
-	for(i = 0; i < stringArray.length; i++) {
-		var innerCount = 0
-		var letters = stringArray[i].split("")
-		for(n = 0; n < letters.length; n++) {
-			for(x = (n + 1); x < letters.length; x++) {
-				if(letters[n] === letters[x]) {
-					innerCount += 1
+	for(var i = 0; i < stringArray.length; i++) {
+		var lettersArray = stringArray[i].split("")
+		for(var n = 0; n < lettersArray.length; n++) {
+			var innerCount = 0
+			var repeatedLetters = []
+			for(var x = (n + 1); x < lettersArray.length; x++) {
+				repeatedLetters.push(lettersArray[n])
+				if(lettersArray[n] === lettersArray[x]) {
+					repeatedLetters.push(lettersArray[x])
 				}
+			}
+			if(repeatedLetters.length > innerCount) {
+				innerCount = repeatedLetters.length
+				repeatedLetters = []
 			}
 		}
 		if(innerCount > count) {
 			count = innerCount
 			innerCount = 0
-			mostRepeats = stringArray[i]
-		}	
+			mostRepeats = stringArray[n]
+		}
 	}
 	return mostRepeats
 }
+
+
+
+
+
+
+
+
+
 
 
 
